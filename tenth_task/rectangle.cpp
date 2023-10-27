fc43acd5ccbe0119d775c99c3459f3cbf9889eda
neveren0ugh <145057257+neveren0ugh@users.noreply.github.com>
2023-10-27 09:18:59 +0300

    Update rectangle.cpp

--- tenth_task/rectangle.cpp
+++ tenth_task/rectangle.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -35,16 +36,44 @@ public:
         top_left.y = t_right.y;
     }
     bool in_rectangle(Coordinate point) {
+        if ((abs(point.x - top_right.x) < 1e-6) and (abs(point.y - top_right.y) < 1e-6))
+            return true;
+        if ((abs(point.x - top_left.x) < 1e-6) and (abs(point.y - top_left.y) < 1e-6))
+            return true;
+        if ((abs(point.x - bottom_left.x) < 1e-6) and (abs(point.y - bottom_left.y) < 1e-6))
+            return true;
+        if ((abs(point.x - bottom_right.x) < 1e-6) and (abs(point.y - bottom_right.y) < 1e-6))
+            return true;
         return ((point.x < top_right.x) and (point.x > top_left.x) and (point.y < top_left.y) and (point.y > bottom_left.y));
     }
     void print() {
         cout << bottom_left.x << bottom_left.y << bottom_right.x << bottom_right.y << top_left.x << top_left.y << top_right.x << top_right.y << "\n";
     }
+    Coordinate get_left_bottom() {
+        return bottom_left;
+    }
+    Coordinate get_right_bottom() {
+        return bottom_right;
+    }
+    Coordinate get_left_top() {
+        return top_left;
+    }
+    Coordinate get_right_top() {
+        return top_right;
+    }
 };
 
 
 int main() {
-    Coordinate nigga{1, 2}, balls{3, 4}, point1{1.5, 3};
-    Rectangle puss{nigga, balls};
+    Coordinate check(1, 1), lb1(0, 0), rt1(6, 5), lb2(-1, -1), rt2(10, 10);
+    Rectangle first(lb1, rt1), second(lb2, rt2);
+    if (first.in_rectangle(second.get_left_bottom()))
+        cout << "Rectangles do overlap";
+    if (first.in_rectangle(second.get_left_top()))
+        cout << "Rectangles do overlap";
+    if (first.in_rectangle(second.get_right_bottom()))
+        cout << "Rectangles do overlap";
+    if (first.in_rectangle(second.get_right_top()))
+        cout << "Rectangles do overlap";
     return 0;
 }
